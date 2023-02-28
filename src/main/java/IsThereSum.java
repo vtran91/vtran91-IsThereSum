1409a992fff2da0e8fc212df58cb2e22d713dbe8
vtran91 <vtran16@bellsouth.net>
2023-02-28 16:10:08 +0000

    Completed IsThereSum Lab

--- src/main/java/IsThereSum.java
+++ src/main/java/IsThereSum.java
@@ -10,6 +10,14 @@ public class IsThereSum {
      * @return true if two separate values in the array add up to a target, false otherwise.
      */
     public boolean check(int[] arr, int target){
+        for (int i = 0; i < arr.length; i++)
+        {
+            for (int j = 1; j < arr.length - 1; j++)
+            {
+                if (arr[i] + arr[j] == target)
+                    return true;
+            }
+        }
         return false;
     }
 }
